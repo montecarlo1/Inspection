a40dc210f4dc515fbc25970a7655a60784cd9738
jacklee0810 <jacklee0810@qq.com>
2013-04-18 09:45:35 +0800

    同一个人不能登录多次和不能三个以上用户登录；
    Signed-off-by: jacklee0810 <jacklee0810@qq.com>

--- SourceCode/src/com/bchd/inspection/ui/Login.java
+++ SourceCode/src/com/bchd/inspection/ui/Login.java
@@ -1,6 +1,9 @@
 package com.bchd.inspection.ui;
 
+import java.util.ArrayList;
+
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 
 import com.bchd.inspection.AppContext;
 import com.bchd.inspection.R;
+import com.bchd.inspection.bean.EmployeeInfo;
 
 public class Login extends BaseActivity {
 	private EditText mUser = null; // 帐号编辑框
@@ -20,25 +24,37 @@ public class Login extends BaseActivity {
 	private ImageButton mbtnEnter  = null; //进入系统按钮
 	private TextView mtvWelcome = null;
 	private AppContext con = null;
-
+	private Toast mToast;
+	private static int m_ClickNum=0;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 
 		prepareView();
-
+		
 		// 设置登录按钮的事件监听器(匿名内部类作为事件监听器类)
 		mbtnLogin.setOnClickListener(new ImageButton.OnClickListener() 
 		{
 			@Override
 			public void onClick(View v) {
 				login_one();
+				 m_ClickNum ++ ;
+				 if(m_ClickNum>4){
+					 Dialog alertDialog = new AlertDialog.Builder(Login.this)
+						.setIcon(R.drawable.ic_launcher)
+						.setTitle("登录失败")
+						.setMessage("您的登录次数过多，\n请重新输入！")
+						.create();
+						alertDialog.show();					 
+				 }
+				 return;
 			}
 		});
 		// 设置进入系统按钮的事件监听器(匿名内部类作为事件监听器类)
 		mbtnEnter.setOnClickListener(new ImageButton.OnClickListener() 
 		{
+			int i=0;
 			@Override
 			public void onClick(View v) {
 				//跳转到主界面
@@ -53,10 +69,11 @@ public class Login extends BaseActivity {
 					.show();
 				}
 				else
-				{	
-					Intent intent = new Intent();
-					intent.setClass(Login.this, MainActivity.class);
-					startActivity(intent);
+				{											 			
+						   Intent intent = new Intent();
+						   intent.setClass(Login.this, MainActivity.class);
+						   startActivity(intent);			  
+					 
 				}
 			}
 		});
@@ -84,6 +101,7 @@ public class Login extends BaseActivity {
 		//Log.e("leilei",mPassword.getText().toString());
 		String userID = mUser.getText().toString();
 		String password = mPassword.getText().toString();
+		
 		if ("".equals(userID)|| "".equals(password))// 判断帐号和密码是否为空
 		{
 			new AlertDialog.Builder(Login.this)
@@ -97,27 +115,39 @@ public class Login extends BaseActivity {
 		else 
 		{
 			try 
-			{
-				if (con.Login(userID, password)) {
-
-					//更新欢迎内容
-					Toast.makeText(Login.this, "欢迎登录： "+ con.getLoginUsersString(true),Toast.LENGTH_SHORT).show();
-					mtvWelcome.setText("欢迎登录： "+ con.getLoginUsersString(false));
-				} 
-				else {
-
-					new AlertDialog.Builder(Login.this)
-							.setIcon(getResources()
-									.getDrawable(
-									R.drawable.login_error_icon))
-							.setTitle("登录失败")
-							.setMessage("员工号或者密码不正确，\n请检查后重新输入！")
-							.create().show();
-				}
-			} catch (Exception e) {
+			{				 
+				    //更新欢迎内容
+					if(con.Login(userID, password)){
+						Toast.makeText(Login.this, "欢迎登录： "+ con.getLoginUsersString(true),Toast.LENGTH_SHORT).show();
+						mtvWelcome.setText("欢迎登录： "+ con.getLoginUsersString(false));
+						 
+					}
+					else if(con.Login(userID, password) && (con.getLoginUsers().size()>2 ||  m_ClickNum>4)){			
+							Dialog alertDialog = new AlertDialog.Builder(this)
+							.setIcon(R.drawable.ic_launcher)
+							.setTitle("温馨提示")
+							.setMessage("登录人数或同一个人登录次数过多！")
+							.create();
+							alertDialog.show();		
+							 
+					}										
+					else{
+			                new AlertDialog.Builder(Login.this)
+										.setIcon(getResources()
+												.getDrawable(
+												R.drawable.login_error_icon))
+										.setTitle("登录失败")
+										.setMessage("员工号或者密码不正确，\n请检查后重新输入！")
+										.create().show();
+					}	
+				 
+			}
+			catch (Exception e) {
 				e.printStackTrace();
-				Toast.makeText(Login.this, e.getMessage(),Toast.LENGTH_LONG).show();
+				Toast.makeText(Login.this, e.getMessage(),Toast.LENGTH_LONG).show();				
 			}
 		}
 	}
+	
+    
 }
